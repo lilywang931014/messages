2817c01eb0c817242fb511ad8824888b82439693
王俐婷 <Lily_Wang@wanglitingdeMacBook-Pro.local>
2019-02-27 16:14:59 -0800

    Use bcrypt to hash and salt passwort

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-var encrypt = require('mongoose-encryption');
+// install and require bcrpt
+//create a saltRounds
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 
 const app = express();
@@ -19,10 +23,6 @@ const userSchema = new mongoose.Schema({
   username:String,
   password:String
 });
-
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,44 +45,53 @@ app.get("/register", function(req,res){
 
 app.post("/register", function(req,res){
 
-  const newUser = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
     if(err){
       console.log(err);
     }else{
-      res.render("message");
+      const newUser = new User({
+        username: req.body.username,
+        password: hash
+      });
+
+      newUser.save(function(err){
+        if(err){
+          console.log(err);
+        }else{
+          res.render("message");
+        }
+      });
     }
   });
-
-
 });
+
+
 
 app.post("/login",function(req,res){
 
   const username = req.body.username;
   const password = req.body.password;
 
+
+
   User.findOne({username: username}, function(err, foundUser){
     if(err){
       console.log(err);
     }else{
-      if(password === foundUser.password){
-        res.render("message");
+      if(foundUser){
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(err){
+            console.log(err);
+          }else{
+            if(result === true){
+              res.render("message");
+            }
+          }
+        });
       }
     }
   });
 });
-
-
-
-
-
-
-
 
 app.listen(3000,function(err){
   if(err){
